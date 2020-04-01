32b91115d9fb0f3fcbfde7ec99a1b1b1d7332c83
Kenny Nguyen <ken_nggg@outlook.com>
2020-04-01 18:41:05 +1300

    finishing touches to web-page

--- src/App.js
+++ src/App.js
@@ -1,41 +1,62 @@
 import React from 'react';
 import AboutPage from './pages/aboutpage/aboutpage.component';
-import styled, { keyframes } from "styled-components";
-import FadeIn from "@bit/formidablelabs.react-animations.fade-in";
 import HomePage from './pages/homepage/homepage.component';
-
-
 import {
-  BrowserRouter as Router,
-  Switch,
   Route,
   Link
 } from "react-router-dom";
-
-
+import Switch from "react-switch"
 import './App.scss';
 
-
-
-const FadeInAnimation = keyframes`${FadeIn}`;
-const FadeInDiv = styled.div`
-  animation: 0.5s ${FadeInAnimation};
-`;
-
-
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      checked: false,
+    }
+    this.props = props;
+  }  
+  handleChange = (checked) => {
+    this.setState({checked})
+  };
   render() {
     return (
-      <div>
-        <FadeInDiv>
-          <Route exact path = '/' component = {HomePage}/>
-          <Route exact path = '/about' component = {AboutPage}/>
-          <Route exact path = '/cv' component = {HomePage}/>
-          <Route exact path = '/projects' component = {HomePage}/>
-        </FadeInDiv>
+      <div className = 'dir'>
+        <div className = {this.state.checked ?  'wrappertrue' : 'wrapperfalse'}>
+          <div className = 'header'>
+            <div className =  'options'>
+              <Link className =  'option' to ='/'>Kenny Nguyen</Link>
+              <Link className =  'option' to ='/about'>About</Link>
+              <Link className =  'option' to ='/'>Projects</Link>
+            </div>
+            <div className = 'switch'>
+              <span className = 'text'>
+              Night Mode
+              </span>
+            <Switch 
+              onChange = {this.handleChange} 
+              checked = {this.state.checked}
+              offColor = '#00000'
+              onHandleColor = '#808080'
+              offHandleColor = '#808080'
+              width = {70}
+              height = {30}
+              />
+            </div>
+          </div>
+            <div className = {'content'}>
+              <Route exact path = '/' component = {HomePage}/>       
+              <Route exact path = '/about' component = {AboutPage}/>       
+            <br/>
+            <footer>
+                COPYRIGHT KENNY NGUYEN 2020
+            </footer>
+            </div>
+          </div>
       </div>
     )
   }
 }
+
 
 export default App;
